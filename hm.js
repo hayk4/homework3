fb972944358e0513d25243fd4dcfbc71bdc54b90
hayk4 <hayksimonyan4@gmail.com>
2017-09-23 23:06:45 +0400

    second commit

--- hm.js
+++ hm.js
@@ -1,49 +1,72 @@
-const func1=function(num1, num2, num3, num4) {
-	return ((num1 + num2) * num3) / num4;
+//A function that takes at least three arguments and returns the result of some set of operations using those arguments 
+const score = function(score1, score2, score3) {
+	return score1 + score2 + score3;
 };
-func1(10, 12, 2, 4);
+const match1 = score (3, 5, 2);
+console.log(match1);
 
-const func2=function() {
-	return 'Have a nice day';
+//A function that takes no arguments and returns something
+const sofarwithme = function() {
+return "oke";
 };
+const lesson1 = sofarwithme();
+console.log (lesson1);
 
-const func3=function(str) {
+//A function that takes arguments, does something but does not return anything 
 
-console.log(str);
-}; 
-func3('Just smile');
-
-const fullName=function(fName, lName) {
-	return fName + ' ' + lName;
+const money = function(money1, money2){
+	
 };
-fullName('John', 'Mayer');
+const kangarumpaheq = money(100, 200);
 
-const strings=function(str1, str2, str3) {
-	if (str1.length>=str2.length && str1.length>str3.length) {
-return str1;
-} else if (str2.length>str3.length) {
-return str2;
+//A function called fullName that takes as argument first name and last name and returns the full name of the person (first name concatenated with last name with a space in between)
+const fullName = function(firstname, lastname); {
+	return firstname +' ' + lastname;
 }
-return str3;
-};
-strings('I', 'love', 'coding');
+const firstname = fullName('Christiano', 'Ronaldo')
+console.log(firstname);
 
-const twoNumbers=function(num1, num2) {
-	if (num1 === num2) {
+//A function that takes three strings and returns the string that is the longest.
+const string = function(x, y, z,) {
+	if (x.length > y.length && x.length > z.length) {
+		return x;
+	}
+if (y.length > x.length && y.length > z.length) {
+	return y;
+	if (z.length > y.length && z.length > x.length) {
+		return z;
+	}
+};
+const word = string('how', 'to', 'pass' )
+console.log word;
+
+//A function that takes two numbers and returns 0 if they are equal, 1 if the first is larger and -1 if the second is larger 
+const ando = function(x, y) {
+	if(x === y) {
 		return 0;
-}   else if (num1>num2) {
+	}
+	if (x > y) {
 		return 1;
-}   else {
-	return -1;
+	}
+	if (x < y) {
+		return -1;
+	}
 };
+const mazmanyan = equal(-5, -4)
+console.log (mazmanyan);
 
-const firstTruethy=function(val1, val2, val3) {
-	if (true === val1) {
-		return val1;
-	} else if (true === val2) {
-		return val2;
-	} else if (true === val3) {
-		return val3;
-}
-	};
-	function('', NaN, 'AUA is home');
+//A function that takes three inputs and returns the first truethy value (example:  firstTruethy(0, '', 1); should return 1)
+
+const truethy = function(x, y, z) {
+	if (x) {
+		return x;
+	}
+	esle if 󰀀 {
+		return y;
+	}
+	if (z) {
+		return z;
+	}
+};
+const razag = truethy(NAN , '1', ' ');
+console.log (razag);
